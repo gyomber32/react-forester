11d9a70c4a6416d904f739ef87702dd0a629e96b
ginger <gyomberattila@gmail.com>
2020-03-09 01:14:37 +0100

    auth problems are still here

--- src/pages/Auth.tsx
+++ src/pages/Auth.tsx
@@ -1,10 +1,17 @@
 import React, { Component } from "react";
 import { Formik, Form, Field, ErrorMessage, FormikHelpers } from "formik";
 import * as Yup from "yup";
+const axios = require("axios")
+
+const httpRequest = axios.create({
+  timeout: 5000,
+  headers: {
+    "Content-Type": "application/json" }
+});
 
 const authSchema = Yup.object().shape({
   password: Yup.string()
-    .min(8, "Too Short!")
+    // .min(8, "Too Short!")
     .max(50, "Too Long!")
     .required("Required"),
   email: Yup.string()
@@ -21,12 +28,50 @@ export class AuthPage extends Component {
   initialValues: AuthFormValues = { email: "", password: "" };
   handleSubmit = (
     values: { email: string; password: string },
-    { setSubmitting }: FormikHelpers<AuthFormValues>
+    { setSubmitting }: FormikHelpers<AuthFormValues>,
+    requestBody = {
+      query: `
+        query {
+          login(email: "${values.email}", password: "${values.password}") {
+            userId
+            token
+            tokenExpiration
+          }
+        }
+      `
+    }
   ) => {
-    setTimeout(() => {
-      alert(JSON.stringify(values, null, 2));
-      setSubmitting(false);
-    }, 400);
+    /*fetch('http://localhost:8000/graphql', {
+      method: 'POST',
+      body: JSON.stringify(requestBody),
+      mode: "cors",
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+      .then(res => {
+        console.log(res);
+        if (res.status !== 200 && res.status !== 201) {
+          throw new Error('Failed!');
+        }
+        return res.json();
+      })
+      .then(resData => {
+        console.log(resData);
+      })
+      .catch(err => {
+        console.log(err);
+      });*/
+    httpRequest
+      .post("http://localhost:8000/graphql", requestBody)
+      .then(response => {
+        console.log(response);
+        setSubmitting(false);
+      })
+      .catch(error => {
+        console.log(error);
+        setSubmitting(false);
+      });
   };
 
   render() {
