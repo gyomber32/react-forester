63b340fe725d45d891253efc31f9927d8cdae9d1
ginger <gyomberattila@gmail.com>
2020-05-14 15:04:03 +0200

    lazy loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,21 @@
-import React from "react";
+import React, { Suspense, Fragment } from "react";
 import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 
-/* pages */
 import AuthPage from "./pages/Authentication/Auth";
 import SeedlingsPage from "./pages/Seedlings/Seedlings";
 import SeedsPage from "./pages/Seeds/Seeds";
-import MapPage from "./pages/Map/Map";
-import PageNotFound from "./pages/PageNotFound/PageNotFound";
+
+import Backdrop from "./components/Backdrop/Backdrop";
+import Spinner from "./components/Spinner/Spinner";
+
+import IsAuthenticated from "./utils/IsAuthenticated";
 
 import styles from "./App.module.scss";
-import IsAuthenticated from "./utils/IsAuthenticated";
+
+const MapPage = React.lazy(() => import("./pages/Map/Map"));
+const PageNotFound = React.lazy(() =>
+  import("./pages/PageNotFound/PageNotFound")
+);
 
 const App = () => {
   return (
@@ -34,13 +40,39 @@ const App = () => {
             <Route
               path="/map"
               render={() =>
-                IsAuthenticated() ? <MapPage /> : <Redirect to="/" />
+                IsAuthenticated() ? (
+                  <Suspense
+                    fallback={
+                      <Fragment>
+                        <Backdrop />
+                        <Spinner />
+                      </Fragment>
+                    }
+                  >
+                    <MapPage />
+                  </Suspense>
+                ) : (
+                  <Redirect to="/" />
+                )
               }
             />
             <Route
               path="*"
               render={() =>
-                IsAuthenticated() ? <PageNotFound /> : <Redirect to="/" />
+                IsAuthenticated() ? (
+                  <Suspense
+                    fallback={
+                      <Fragment>
+                        <Backdrop />
+                        <Spinner />
+                      </Fragment>
+                    }
+                  >
+                    <PageNotFound />
+                  </Suspense>
+                ) : (
+                  <Redirect to="/" />
+                )
               }
             />
           </Switch>
