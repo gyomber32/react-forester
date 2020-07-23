7b1083d8059f3bdf27c6ddaa0dab7d596dd55374
Attila Gyomber <attila.gyomber@jaystack.com>
2020-07-23 13:34:12 +0200

    call hooks in useCallback

--- src/pages/Trees/Trees.tsx
+++ src/pages/Trees/Trees.tsx
@@ -1,185 +1,166 @@
-import React, { Fragment, useState, useEffect } from "react";
+import React, { Fragment, useState, useEffect, useCallback } from 'react';
 
-import Card from "../../components/Card/Card";
-import DetailsModal from "../../components/DetailsModal/DetailsModal";
-import ConfirmationModal from "../../components/ConfirmationModal/ConfirmationModal";
-import AddModal from "../../components/AddModal/AddModal";
-import Backdrop from "../../components/Backdrop/Backdrop";
-import AddButton from "../../components/AddButton/AddButton";
-import Navigation from "../../components/Navigation/Navigation";
-import Popup from "../../components/Popup/Popup";
-import Spinner from "../../components/Spinner/Spinner";
-import Chart from "../../components/Chart/Chart";
-import NoData from "../../components/NoData/NoData";
+import Card from '../../components/Card/Card';
+import DetailsModal from '../../components/DetailsModal/DetailsModal';
+import ConfirmationModal from '../../components/ConfirmationModal/ConfirmationModal';
+import AddModal from '../../components/AddModal/AddModal';
+import Backdrop from '../../components/Backdrop/Backdrop';
+import AddButton from '../../components/AddButton/AddButton';
+import Navigation from '../../components/Navigation/Navigation';
+import Popup from '../../components/Popup/Popup';
+import Spinner from '../../components/Spinner/Spinner';
+import Chart from '../../components/Chart/Chart';
+import NoData from '../../components/NoData/NoData';
 
-import Tree from "../../models/types/Tree";
-import PopUp from "../../models/types/PopUp";
+import Tree from '../../models/types/Tree';
+import PopUp from '../../models/types/PopUp';
 
-import {
-  getAllTrees,
-  getOneTree,
-  removeTree,
-  createTree,
-} from "../../api/index";
+import { getAllTrees, getOneTree, removeTree, createTree } from '../../api/index';
 
-import styles from "./Trees.module.scss";
+import styles from './Trees.module.scss';
+import { useCreateTree } from '../../hooks/createTree';
 
-const TreesPage: React.FC = (props) => {
+const TreesPage: React.FC = props => {
   const [trees, setTrees] = useState<Tree[]>([]);
-  const [selectedTree, setSelectedTree] = useState<Tree>({
-    _id: "",
-    picture: "",
-    pictureId: "",
-    species: "",
-    plantedQuantity: 0,
-    survivedQuantity: 0,
-    datePlanted: "",
-    daysInSoil: "",
-    location: "",
-  });
-  const [detailsModal, setDetailsModalState] = useState<boolean>(false);
+  const [selectedTree, setSelectedTree] = useState<Tree>({} as Tree);
+  const [detailsModal, setDetailsModal] = useState<boolean>(false);
   const [addModal, setAddModalState] = useState<boolean>(false);
-  const [confirmationModal, setConfirmationModalState] = useState<boolean>(
-    false
+  const [confirmationModal, setConfirmationModal] = useState<boolean>(false);
+  const [popup, setPopup] = useState<PopUp>({ isOpen: false, message: '' });
+  const [loading, setLoading] = useState<boolean>(false);
+
+  const fetchOneTree = useCallback(
+    async (id: string) => {
+      setLoading(true);
+      try {
+        const tree = await getOneTree(id);
+        setTrees([...trees, tree]);
+      } catch (error) {
+        setPopup({
+          isOpen: true,
+          message: error.message
+        });
+        setTimeout(() => {
+          setPopup({ isOpen: false, message: '' });
+        }, 5500);
+        console.log(error);
+      }
+      setLoading(false);
+    },
+    [getOneTree]
   );
-  const [popup, setPopup] = useState<PopUp>({ isOpen: false, message: "" });
-  const [loading, setLoadingState] = useState<boolean>(false);
 
-  const fetchOneTree = async (id: string) => {
-    setLoadingState(true);
-    try {
-      const tree = await getOneTree(id);
-      setTrees([...trees, tree]);
-    } catch (error) {
-      setPopup({
-        isOpen: true,
-        message: error.message,
-      });
-      setTimeout(() => {
-        setPopup({ isOpen: false, message: "" });
-      }, 5500);
-      console.log(error);
-    }
-    setLoadingState(false);
-  };
-
-  const fetchAllTrees = async () => {
-    setLoadingState(true);
+  const fetchAllTrees = useCallback(async () => {
+    setLoading(true);
     try {
       const trees = await getAllTrees();
       setTrees(trees);
     } catch (error) {
       setPopup({
         isOpen: true,
-        message: error.message,
+        message: error.message
       });
       setTimeout(() => {
-        setPopup({ isOpen: false, message: "" });
+        setPopup({ isOpen: false, message: '' });
       }, 5500);
       console.log(error);
     }
-    setLoadingState(false);
-  };
+    setLoading(false);
+  }, [getAllTrees]);
 
   useEffect(() => {
-    document.addEventListener("keydown", closeOnEscapeButton, false);
+    document.addEventListener('keydown', closeOnEscapeButton, false);
     fetchAllTrees();
     return () => {
-      document.removeEventListener("keydown", closeOnEscapeButton, false);
+      document.removeEventListener('keydown', closeOnEscapeButton, false);
     };
   }, []);
 
-  const closeOnEscapeButton = (event?: any) => {
+  const closeOnEscapeButton = useCallback((event?: any) => {
     if (event.keyCode === 27) {
       closeDetailsModal();
       closeAddModal();
       closeConfirmationModal();
     }
-  };
+  }, []);
 
-  const openDetailsModal = (id: string) => {
-    const tree = trees.filter((tree) => tree._id === id);
-    setSelectedTree(tree[0]);
-    setDetailsModalState(true);
-  };
+  const openDetailsModal = useCallback(
+    (id: string) => {
+      const tree = trees.filter(tree => tree._id === id)[0];
+      setSelectedTree(tree);
+      setDetailsModal(true);
+    },
+    [setDetailsModal]
+  );
 
-  const closeDetailsModal = () => {
-    setDetailsModalState(false);
-    setSelectedTree({
-      _id: "",
-      picture: "",
-      pictureId: "",
-      species: "",
-      plantedQuantity: 0,
-      survivedQuantity: 0,
-      datePlanted: "",
-      daysInSoil: "",
-      location: "",
-    });
-  };
+  const closeDetailsModal = useCallback(() => {
+    setDetailsModal(false);
+    setSelectedTree({} as Tree);
+  }, [setDetailsModal]);
 
-  const openAddModal = () => {
+  const openAddModal = useCallback(() => {
     setAddModalState(true);
-  };
+  }, [setAddModalState]);
 
-  const closeAddModal = () => {
+  const closeAddModal = useCallback(() => {
     setAddModalState(false);
-  };
+  }, [setAddModalState]);
 
-  const openConfirmationModal = () => {
-    setConfirmationModalState(true);
-  };
+  const openConfirmationModal = useCallback(() => {
+    setConfirmationModal(true);
+  }, [setConfirmationModal]);
 
-  const closeConfirmationModal = () => {
-    setConfirmationModalState(false);
+  const closeConfirmationModal = useCallback(() => {
+    setConfirmationModal(false);
     if (detailsModal) {
       closeDetailsModal();
     }
-  };
+  }, [setConfirmationModal]);
 
-  const deleteTree = async () => {
-    setLoadingState(true);
+  const deleteTree = useCallback(async () => {
+    setLoading(true);
     closeConfirmationModal();
     try {
       const responseMessage = await removeTree(selectedTree);
       await fetchAllTrees();
       setPopup({
         isOpen: true,
-        message: responseMessage,
+        message: responseMessage
       });
       setTimeout(() => {
-        setPopup({ isOpen: false, message: "" });
+        setPopup({ isOpen: false, message: '' });
       }, 5500);
     } catch (error) {
       setPopup({ isOpen: true, message: error.message });
       setTimeout(() => {
-        setPopup({ isOpen: false, message: "" });
+        setPopup({ isOpen: false, message: '' });
       }, 5500);
       console.log(error);
     }
-    setLoadingState(false);
-  };
+    setSelectedTree({} as Tree);
+    setLoading(false);
+  }, [removeTree, selectedTree]);
 
-  const onSubmit = async (value: any) => {
-    setLoadingState(true);
+  const onSubmit = useCallback(async (value: any) => {
+    setLoading(true);
     closeAddModal();
     value.survivedQuantity = value.plantedQuantity;
     try {
       const id = await createTree(value);
       await fetchOneTree(id);
-      setPopup({ isOpen: true, message: "Tree created successfully" });
+      setPopup({ isOpen: true, message: 'Tree created successfully' });
       setTimeout(() => {
-        setPopup({ isOpen: false, message: "" });
+        setPopup({ isOpen: false, message: '' });
       }, 5500);
     } catch (error) {
       setPopup({ isOpen: true, message: error.message });
       setTimeout(() => {
-        setPopup({ isOpen: false, message: "" });
+        setPopup({ isOpen: false, message: '' });
       }, 5500);
       console.log(error);
     }
-    setLoadingState(false);
-  };
+    setLoading(false);
+  }, []);
 
   return (
     <Fragment>
@@ -224,10 +205,7 @@ const TreesPage: React.FC = (props) => {
         {confirmationModal && (
           <Fragment>
             <Backdrop click={closeConfirmationModal} zIndex={3}></Backdrop>
-            <ConfirmationModal
-              onYes={deleteTree}
-              onCancel={closeConfirmationModal}
-            ></ConfirmationModal>
+            <ConfirmationModal onYes={deleteTree} onCancel={closeConfirmationModal}></ConfirmationModal>
           </Fragment>
         )}
         {addModal && <Backdrop click={closeAddModal}></Backdrop>}
